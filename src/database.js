6653fef022d5e57379737a5ea8a8f8672ceb5d14
tom <tom@MacBookPro.homenet.telecomitalia.it>
2025-01-01 18:33:20 +0100

    envv-

--- src/database.js
+++ src/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const MONGO_URI = process.env.MONGO_URI;
+const MONGO_URI = "mongodb://localhost/start2impact";
 
 const connectDB = async () => {
     try {
